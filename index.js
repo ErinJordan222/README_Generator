d9065f52db123a7b4f750ac5bd0d8ea626d8bed4
ErinJordan222 <erin.jordan@du.edu>
2022-04-21 14:33:47 -0600

    fixed_writetoFile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -63,9 +63,10 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((inquirerResponses) => {
+    inquirer.prompt(questions)
+    .then((inquirerResponses) => {
         console.log('Generate README...');
-        writetoFile('README.md', generateMarkdown({ ...inquirerResponses}));
+        fs.writetoFile('./README.md', generateMarkdown({ ...inquirerResponses}));
     });
 }
 
